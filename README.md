333e6a2b7192092553733077d30c863f0d5d7ab0
DennyHsieh <tc.hsiehhsieh@gmail.com>
2019-04-14 02:34:37 +0800

    finish attack

--- README.md
+++ README.md
@@ -14,4 +14,163 @@ Decrypt the whole cipher text.
 5468697320697320616e20495634353680df47c094098bf18b0674c8efe1d97002c535a78e9829a961549254f2eab2d86286f73f839b63f2ad9ac24e8307ecab208699d3e58f8627339a4c9111c73616381bda08bb53e5dbbb86dc4f2d0418350f8100b5502ec5dc8a608e31445f0e955e123ffa80d6ae86bce208ecef64f205
 ```
 
-TODO: Need to submit the answer, your code and a document to explain how it works.
+- IV_plaintext(hex): 5468697320697320616e204956343536
+- The IV is: This is an IV456
+
+- Execute log and result
+
+```
+The whole ciphertext is:
+b'5468697320697320616e20495634353680df47c094098bf18b0674c8efe1d97002c535a78e9829a961549254f2eab2d86286f73f839b63f2ad9ac24e8307ecab208699d3e58f8627339a4c9111c73616381bda08bb53e5dbbb86dc4f2d0418350f8100b5502ec5dc8a608e31445f0e955e123ffa80d6ae86bce208ecef64f205'
+
+Directly decrypt the ciphertext, and we can see the plaintext of IV of first 16 bytes
+b"This is an IV456\x80\xdfG\xc0\x94\t\x8b\xf1\x8b\x06t\xc8\xef\xe1\xd9p\x02\xc55\xa7\x8e\x98)\xa9aT\x92T\xf2\xea\xb2\xd8b\x86\xf7?\x83\x9bc\xf2\xad\x9a\xc2N\x83\x07\xec\xab \x86\x99\xd3\xe5\x8f\x86'3\x9aL\x91\x11\xc76\x168\x1b\xda\x08\xbbS\xe5\xdb\xbb\x86\xdcO-\x04\x185\x0f\x81\x00\xb5P.\xc5\xdc\x8a`\x8e1D_\x0e\x95^\x12?\xfa\x80\xd6\xae\x86\xbc\xe2\x08\xec\xefd\xf2\x05"
+
+======
+Start to try block 8
+The IV(temp) encrypted text we need to change elements is:0f8100b5502ec5dc8a608e31445f0e95
+The block we want to try is:5e123ffa80d6ae86bce208ecef64f205
+possible padding size is: 1
+possible padding size is: 8
+good padding size is: 8
+'e\x08\x08\x08\x08\x08\x08\x08\x08'
+'2e\x08\x08\x08\x08\x08\x08\x08\x08'
+'32e\x08\x08\x08\x08\x08\x08\x08\x08'
+'732e\x08\x08\x08\x08\x08\x08\x08\x08'
+'5732e\x08\x08\x08\x08\x08\x08\x08\x08'
+'65732e\x08\x08\x08\x08\x08\x08\x08\x08'
+'d65732e\x08\x08\x08\x08\x08\x08\x08\x08'
+'6d65732e\x08\x08\x08\x08\x08\x08\x08\x08'
+If remove padding off, the last block is:
+'6d65732e'
+
+======
+Start to try block 7
+The IV(temp) encrypted text we need to change elements is:381bda08bb53e5dbbb86dc4f2d041835
+The block we want to try is:0f8100b5502ec5dc8a608e31445f0e95
+'9'
+'69'
+'469'
+'7469'
+'07469'
+'207469'
+'6207469'
+'66207469'
+'f66207469'
+'6f66207469'
+'06f66207469'
+'206f66207469'
+'4206f66207469'
+'74206f66207469'
+'374206f66207469'
+'7374206f66207469'
+
+======
+Start to try block 6
+The IV(temp) encrypted text we need to change elements is:208699d3e58f8627339a4c9111c73616
+The block we want to try is:381bda08bb53e5dbbb86dc4f2d041835
+'2'
+'72'
+'f72'
+'6f72'
+'76f72'
+'776f72'
+'0776f72'
+'20776f72'
+'520776f72'
+'6520776f72'
+'86520776f72'
+'686520776f72'
+'4686520776f72'
+'74686520776f72'
+'074686520776f72'
+'2074686520776f72'
+
+======
+Start to try block 5
+The IV(temp) encrypted text we need to change elements is:6286f73f839b63f2ad9ac24e8307ecab
+The block we want to try is:208699d3e58f8627339a4c9111c73616
+'3'
+'73'
+'173'
+'6173'
+'76173'
+'776173'
+'0776173'
+'20776173'
+'420776173'
+'7420776173'
+'97420776173'
+'697420776173'
+'0697420776173'
+'20697420776173'
+'c20697420776173'
+'2c20697420776173'
+
+======
+Start to try block 4
+The IV(temp) encrypted text we need to change elements is:02c535a78e9829a961549254f2eab2d8
+The block we want to try is:6286f73f839b63f2ad9ac24e8307ecab
+'3'
+'73'
+'573'
+'6573'
+'d6573'
+'6d6573'
+'96d6573'
+'696d6573'
+'4696d6573'
+'74696d6573'
+'074696d6573'
+'2074696d6573'
+'62074696d6573'
+'662074696d6573'
+'f662074696d6573'
+'6f662074696d6573'
+
+======
+Start to try block 3
+The IV(temp) encrypted text we need to change elements is:80df47c094098bf18b0674c8efe1d970
+The block we want to try is:02c535a78e9829a961549254f2eab2d8
+'0'
+'20'
+'420'
+'7420'
+'37420'
+'737420'
+'5737420'
+'65737420'
+'265737420'
+'6265737420'
+'06265737420'
+'206265737420'
+'5206265737420'
+'65206265737420'
+'865206265737420'
+'6865206265737420'
+
+======
+Start to try block 2
+The IV(temp) encrypted text we need to change elements is:5468697320697320616e204956343536
+The block we want to try is:80df47c094098bf18b0674c8efe1d970
+'4'
+'74'
+'074'
+'2074'
+'32074'
+'732074'
+'1732074'
+'61732074'
+'761732074'
+'7761732074'
+'07761732074'
+'207761732074'
+'4207761732074'
+'74207761732074'
+'974207761732074'
+'4974207761732074'
+The plaintext_hex is: 497420776173207468652062657374206f662074696d65732c206974207761732074686520776f727374206f662074696d65732e
+The IV is: This is an IV456
+The plaintext is: It was the best of times, it was the worst of times.
+```
+
--- client.py
+++ client.py
@@ -1,20 +1,197 @@
 import telnetlib
+import binascii
 
-# HOST = "localhost"
-HOST = "140.122.185.174"
 
-tn = telnetlib.Telnet(HOST, 8081)
-tn.read_until(b"---\n")
-ciphertext = tn.read_until(b"---\n")[:-5]
+def query(msg_test):
+    '''ask the server if the ciphertext is valid'''
+    tn.write(msg_test.encode("utf-8") + b'\r\n')
 
-print(ciphertext)
+    ret = tn.read_until(b".\n", timeout=3)
 
-tn.read_until(b"What is your ciphertext?\n")
+    if ret.find(b"invalid") != -1:
+        # print(b"invalid")
+        return False
+    elif ret.find(b"valid") != -1:
+        # print(b"valid")
+        return True
+    else:
+        print(ret)
 
-tn.write(ciphertext + b'\n')
-ret = tn.read_until(b"padding\n")
 
-if ret.find(b"invalid") != -1:
-    print(b"invalid")
-else:
-    print(b"valid")
+def to_hex(s):
+    '''string to hex'''
+    return ''.join("{:02x}".format(ord(c)) for c in s)
+
+
+def to_str(s):
+    '''hex to string'''
+    return s and chr(int(s[:2], base=16)) + to_str(s[2:]) or ''
+
+
+def int2hex(i):
+    return hex(i)[2:] if len(hex(i)[2:]) == 2 else '0' + hex(i)[2:]
+
+
+def exor_pad(i):
+    '''try integer 1 to 16
+    :return 16 byte str with padding 0 in the front'''
+    assert (i > 0)
+    assert (i <= 16)
+    return '00' * (16 - i) + int2hex(i) * i
+
+
+def exor_guess(guess, pos):
+    '''
+    :param guess: guess integer
+    :param pos: position of XOR (0-15)
+    :return: 16 byte str with padding 0 in the front and end
+    '''
+    assert (pos >= 0)
+    assert (pos < 16)
+    return '00' * (15 - pos) + int2hex(guess) + '00' * pos
+
+
+def refill_zero(s):
+    '''
+    append 0 to front string until len(str) = 32
+    :return: str
+    '''
+    return '0' * (32 - len(s)) + s
+
+
+def strxor(a, b):
+    '''
+    xor two strings with different lengths
+    Note: a XOR b in python: chr(ord(a) ^ ord(b))
+    :return: XOR string result
+    '''
+    if len(a) > len(b):
+        return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a[:len(b)], b)])
+    else:
+        return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a, b[:len(a)])])
+
+
+def hex_xor(s1, s2):
+    '''
+    xor two hex by decode string to hex
+    :return: XOR string result
+    '''
+    return to_hex(strxor(to_str(s1), to_str(s2)))
+
+
+def test_a_byte(found_msg, pos, dictionary_, iv, ct, is_padding=False):
+    '''
+    Try a byte to get the plaintext
+    :param found_msg: the hex_str has already been found
+    :param pos: byte position of hex backward from 1-16 which will be tried by int between 0-255
+    :param dictionary_: int range between 0-255
+    :param iv: hex_str to be XORed
+    :param ct: ciphertext to be decrypted
+    :param is_padding: check the padding validity of ciphertext, default=False
+    :return: hex(plaintext)
+    '''
+    pad = exor_pad(pos)
+    lastmsg = refill_zero(to_hex(found_msg))
+    getletter = False
+    possible_padding = []
+    for guess in dictionary_:
+        guess_pad = exor_guess(guess, pos - 1)
+        # Try all possible situation
+        send_ek = hex_xor(lastmsg, hex_xor(iv, hex_xor(guess_pad, pad))) + ct
+        # while len(send_ek) < 256:
+        #     send_ek = '00' + send_ek
+        # print(send_ek.encode("utf-8"))
+        if query(send_ek):
+            getletter = True
+            new_msg = to_str(int2hex(guess)) + found_msg
+            if is_padding:
+                possible_padding.append(guess)
+            else:
+                return new_msg
+    if is_padding:
+        return possible_padding
+    if getletter is False:
+        return None
+
+
+if __name__ == "__main__":
+    HOST = "140.122.185.174"
+
+    tn = telnetlib.Telnet(HOST, 8081)
+    tn.read_until(b"---\n")
+    ciphertext = tn.read_until(b"---\n")[:-5]
+    ciphertext_decrypt = binascii.unhexlify(ciphertext)
+
+    print("The whole ciphertext is:")
+    print(ciphertext)
+    print("\nDirectly decrypt the ciphertext, and we can see the plaintext of IV of first 16 bytes")
+    print(ciphertext_decrypt)
+
+    tn.read_until(b"What is your ciphertext?\n")
+
+    N = 16
+    printable_chars = range(32, 128)
+    padding_chars = range(1, 17)
+    inter_text = ''
+
+    #  Change to Dec
+    ek_liststr = list(ciphertext)
+    # print(ek_liststr)
+    # print(type(ek_liststr[-1]))
+
+    iv_lt = ek_liststr[:32]
+    iv_origin = bytes(iv_lt)
+
+    # Divide ciphertext into block
+    blocks = []
+    ciphertext_decode = ciphertext.decode("utf-8")
+    while ciphertext_decode:
+        blocks.append(ciphertext_decode[:32])
+        ciphertext_decode = ciphertext_decode[32:]
+    # print(blocks)
+
+    for index in range(1, 8):
+        b_index = 8 - index
+        print("\n======")
+        print('Start to try block', b_index + 1)
+        iv = blocks[b_index - 1]
+        block = blocks[b_index]
+        print("The IV(temp) encrypted text we need to change elements is:" + iv)
+        print("The block we want to try is:" + block)
+
+        # Try the validity of the padding for the last block
+        is_last_block = False
+        if b_index == len(blocks)-1:
+            is_last_block = True
+        if is_last_block:
+            possible_paddings = test_a_byte('', 1, padding_chars, iv, block, True)
+            # Test the validity of the padding after chosen
+            for i in possible_paddings:
+                print("possible padding size is:", i)
+                msg = chr(i) * i
+                start_byte = i + 1
+                if test_a_byte(msg, start_byte, printable_chars, iv, block) is not None:
+                    print("good padding size is:", i)
+                    break
+        else:
+            msg = ''
+            start_byte = 1
+
+        # Decrypted the byte one by one to the chosen block of 16byte
+        for pos in range(start_byte, 17):
+            is_found = test_a_byte(msg, pos, printable_chars, iv, block)
+            if is_found:
+                msg = is_found
+                print("%r" % msg)
+            else:
+                print("can't found the last #%d byte" % pos)
+                exit(0)
+            if pos == 16:
+                inter_text = msg + inter_text
+        if is_last_block and msg:
+            print("If remove padding off, the last block is:\n%r" % msg[:-(start_byte - 1)])
+            inter_text = msg[:-(start_byte - 1)]
+
+    print("The plaintext_hex is:", inter_text)
+    print("The IV is:", to_str(iv_origin))
+    print("The plaintext is:", to_str(inter_text))
